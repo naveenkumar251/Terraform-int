ede47ce9e65c95a140a896357547811df19cc1e2
naveenkumar251 <91377442+naveenkumar251@users.noreply.github.com>
2021-10-06 18:23:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
         profile = "default"
         region  = "us-east-1"
   }
-resource "aws_instance" {
+resource "aws_instance" "myec2" {
   ami           = "ami-5b41123e"
   instance_type = "t2.micro"
   subnet_id = "subnet-06f06aa686014295b"
-  name = $var.name
+  name = var.name
 }
 
