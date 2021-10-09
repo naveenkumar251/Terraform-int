4589fbba0ded8dd5f89eb0a39a5115fecda464af
naveenkumar251 <91377442+naveenkumar251@users.noreply.github.com>
2021-10-09 17:28:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,9 @@ resource "aws_instance" "myec2" {
   ami           = "ami-5b41123e"
   instance_type = "t2.micro"
   subnet_id = "subnet-06f06aa686014295b"
+ Name = "var.ec2name"
   
-tags  = {
-     Name = "var.ec2name"
-  }
+#tags  = {
+ #    Name = "var.ec2name"
+ # }
  }
